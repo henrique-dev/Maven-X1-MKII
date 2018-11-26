7c3fc0eb39a83c8bfce0c8a970721627483a2476
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-11-26 12:00:23 -0300

    implementação do sistema de centro de gravidade

--- Raspberry/src/com/br/phdev/GravitySystem.java
+++ Raspberry/src/com/br/phdev/GravitySystem.java
@@ -11,38 +11,68 @@ public class GravitySystem {
     private double height;
     private Vector2D center;
 
+    private GravityCell leftGravityCell;
+    private GravityCell rightGravityCell;
+
     public GravitySystem(Body body, double width, double height) {
         this.width = width;
         this.height = height;
         double cx = body.getArea().x / 2;
         double cy = body.getArea().y / 2;
         this.center = new Vector2D(cx, cy);
+
+        this.leftGravityCell = new GravityCell(
+                new Vertex(new Vector2D(cx - width/2, cy - height / 2), body.getLeg(Body.LEG_FRONT_LEFT)),
+                new Vertex(new Vector2D(cx + width / 2, cy), body.getLeg(Body.LEG_MID_RIGHT)),
+                new Vertex(new Vector2D(cx - width / 2, cy - height / 2), body.getLeg(Body.LEG_BACK_LEFT))
+        );
+
+        this.rightGravityCell = new GravityCell(
+                new Vertex(new Vector2D(cx + width / 2, cy + height / 2), body.getLeg(Body.LEG_FRONT_RIGHT)),
+                new Vertex(new Vector2D(cx - width / 2, cy), body.getLeg(Body.LEG_MID_LEFT)),
+                new Vertex(new Vector2D(cx + width / 2, cy - height / 2), body.getLeg(Body.LEG_BACK_RIGHT))
+        );
+
         Log.w("Centro de gravidade em (" + cx + "," + cy + ")");
-    }
-
-
-    public void setVertexLeg(int vertex, Leg leg) {
-        switch (vertex) {
-            case 0:
-
-        }
+        Log.w("Celula esquerda: " + this.leftGravityCell.toString());
+        Log.w("Celula direita: " + this.rightGravityCell.toString());
     }
 
     private class GravityCell {
 
+        Vertex top;
+        Vertex mid;
+        Vertex bottom;
 
-
-        class Vertex {
-
-            Vector2D vertex;
-            Leg leg;
-
-            public Vertex(Vector2D vertex, Leg leg) {
-                this.vertex = vertex;
-                this.leg = leg;
-            }
+        GravityCell(Vertex top, Vertex mid, Vertex bottom) {
+            this.top = top;
+            this.mid = mid;
+            this.bottom = bottom;
         }
 
+        @Override
+        public String toString() {
+            return "Top vertex-> " + top.toString() + "\n" +
+                    "Mid vertex-> " + mid.toString() + "\n" +
+                    "Bottom vertex-> " + bottom.toString();
+        }
+
+    }
+
+    private class Vertex {
+
+        Vector2D vertex;
+        Leg leg;
+
+        Vertex(Vector2D vertex, Leg leg) {
+            this.vertex = vertex;
+            this.leg = leg;
+        }
+
+        @Override
+        public String toString() {
+            return "Leg number: " + leg.getLegData().getLegNumber() + "  V(" + vertex.x + "," + vertex.y + ")";
+        }
     }
 
 }
