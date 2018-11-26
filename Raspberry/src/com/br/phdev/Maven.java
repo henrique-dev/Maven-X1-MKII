c0f9398bd00577189472200ce33365a5a2036739
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-11-26 14:58:40 -0300

    implementação do sistema de centro de gravidade

--- Raspberry/src/com/br/phdev/Maven.java
+++ Raspberry/src/com/br/phdev/Maven.java
@@ -537,16 +537,12 @@ public class Maven {
 					        int index = values.indexOf(" ");
 					        String value = values.substring(0, index);
 					        double width = Double.parseDouble(value);
-                            System.out.println(width);
-					        values = values.substring(index);
-                            System.out.println(values);
+					        values = values.substring(index+1);
 					        index = values.indexOf(" ");
 					        value = values.substring(0, index);
 					        double height = Double.parseDouble(value);
-                            System.out.println(height);
-					        value = values.substring(index);
+					        value = values.substring(index+1);
 					        double precision = Double.parseDouble(value);
-                            System.out.println(precision);
                             System.out.println(width + height + precision);
                             //Log.w("Iniciando sistema de centro de gravidade com medida padrão 430mmx430mm");
                             //robotSystem.initGravitySystem(430, 430);
