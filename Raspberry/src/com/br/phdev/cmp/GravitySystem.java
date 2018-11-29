046488c37bef66e8f65353d481139eee561de289
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-11-29 17:16:52 -0300

    implementação de movimentação

--- Raspberry/src/com/br/phdev/cmp/GravitySystem.java
+++ Raspberry/src/com/br/phdev/cmp/GravitySystem.java
@@ -70,11 +70,11 @@ public class GravitySystem  {
         waitForAnotherCell();
         lock2.unlock();
         Log.s("Celula executou o movimento");
-        lock2.lock();
+        //lock2.lock();
         //leftGravityCell.adjustLegToVertex(vector2D, true, 1000, false, null);
-        rightGravityCell.adjustLegToVertex(vector2D, true, 1000, false, waitingTaskCellListener);
-        waitForAnotherCell();
-        lock2.unlock();
+        //rightGravityCell.adjustLegToVertex(vector2D, true, 1000, false, waitingTaskCellListener);
+        //waitForAnotherCell();
+        //lock2.unlock();
         Log.s("Celula executou o movimento");
     }
 
