1af3b49d5c6b26d534ec0f9a3b4291d37ec3e488
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-11-28 22:56:08 -0300

    implementação do sistema de centro de gravidade

--- Raspberry/src/com/br/phdev/cmp/GravitySystem.java
+++ Raspberry/src/com/br/phdev/cmp/GravitySystem.java
@@ -86,12 +86,12 @@ public class GravitySystem  {
 
         void initCell() {
 
-
+/*
             top.init();
             mid.init();
-            bottom.init();
+            bottom.init();*/
 
-            /*
+
             List<Task> servoTaskList = new ArrayList<>();
             double cw;
             double ch;
@@ -191,7 +191,7 @@ public class GravitySystem  {
             Log.s("Comprimento novo da perna: " + bottom.leg.getTarsus().getFinalVector().subtract(bottom.leg.getBase().getOriginVector()).getSize());
             System.out.println();
 
-            this.moving = false;*/
+            this.moving = false;
         }
 
         private void adjust(Vector2D vector2D) {
