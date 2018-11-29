3b0833802d88e01226b2cc657eda4ed4d8b416df
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-11-29 00:11:26 -0300

    implementação do sistema de centro de gravidade

--- Raspberry/src/com/br/phdev/cmp/GravitySystem.java
+++ Raspberry/src/com/br/phdev/cmp/GravitySystem.java
@@ -148,7 +148,11 @@ public class GravitySystem  {
             double ldegrees = Math.toDegrees(Math.asin(lsin));
 
             //double angle = vdegrees - ldegrees;
-            Log.e(Math.toDegrees(Math.asin(Math.sin(leg.getLegData().getLegMidDegrees()))));
+            double sin = Math.sin(leg.getLegData().getLegMidDegrees());
+            double asin = Math.asin(Math.sin(sin));
+            Log.e(sin);
+            Log.e(asin);
+            Log.e(Math.toDegrees(asin));
             Log.e(leg.getLegData().getLegMidDegrees());
             double angle = vdegrees - Math.toDegrees(Math.asin(Math.sin(leg.getLegData().getLegMidDegrees())));
 
