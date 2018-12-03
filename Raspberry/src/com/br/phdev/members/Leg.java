a605047d89a60ab384a054c6901609ea88c98f35
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-12-03 15:46:17 -0300

    aprimoramento de movimentação

--- Raspberry/src/com/br/phdev/members/Leg.java
+++ Raspberry/src/com/br/phdev/members/Leg.java
@@ -247,7 +247,7 @@ public class Leg {
         TaskGroup taskGroups = elevate ? new TaskGroup(new int[]{1, 4}) : new TaskGroup(new int[]{4});
         
         if (elevate) {
-            double currentElevateAngle = femur.getCurrentAngle() + 40;
+            double currentElevateAngle = femur.getCurrentAngle() + 25;
 
             servoTaskList.add(new ServoTask(
                     this.femur.getServo(),
@@ -315,6 +315,7 @@ public class Leg {
     }
 
     public void stay() {
+        this.base.getServo().move(base.getCurrentAngle());
         this.base.getServo().setRawPosition(0);
         this.femur.move(femur.getCurrentAngle());
         this.tarsus.move(tarsus.getCurrentAngle());
