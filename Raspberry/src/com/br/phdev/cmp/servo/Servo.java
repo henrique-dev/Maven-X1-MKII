46547ee209063c18e7bc00ec1e00a2b8f7fe68d6
Paulo Henrique <23532516+henrique-dev@users.noreply.github.com>
2018-12-07 12:56:06 -0300

    aprimoramento de movimentação

--- Raspberry/src/com/br/phdev/cmp/servo/Servo.java
+++ Raspberry/src/com/br/phdev/cmp/servo/Servo.java
@@ -22,6 +22,8 @@ public class Servo {
     }
 
     private void initServo() {
+        this.setRawPosition(0);
+        waitFor(100);
         this.move(0, false);
         waitFor(300);
         this.setRawPosition(0);
